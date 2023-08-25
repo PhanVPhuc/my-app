9c9e269da4ba151ef6e0370a21f960421ad16551
VinhPhuc <pphuc1996@gmail.com>
2023-08-25 16:15:36 +0700

    ?

---
+++ src/component/Numberlist.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+//   const number = [1, 2, 3, 4, 5];
+
+// function Number() {
+//   function NumberList() {
+//     const number = props.number;
+
+//     function ListItems() {
+//       return number.map((number) => {
+//         // có tính toán trong map thì thực hiện ở đây
+//         return <li key={number.toString()}>{number}</li>;
+//       });
+//     }
+//     return <ul>{listItems()}</ul>;
+//   }
+// }
+
+// export default NumberListumberList;
