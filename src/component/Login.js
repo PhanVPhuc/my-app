b288de6b4b4dc6d8eb2dcd213af2c7212659da19
VinhPhuc <pphuc1996@gmail.com>
2023-09-07 10:50:23 +0700

    a dung roi login

--- src/component/Login.js
+++ src/component/Login.js
@@ -54,7 +54,7 @@ function Login() {
   function renderError() {
     if (Object.keys(errors).length > 0) {
       return Object.keys(errors).map((key, index) => {
-        return <li key={index}> {error[key]}</li>;
+        return <li key={index}> {errors[key]}</li>;
       });
     }
   }
