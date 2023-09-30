be2a7e41bd26fe7d1bfbe127d36e2ee55be43898
VinhPhuc <pphuc1996@gmail.com>
2023-09-30 10:03:56 +0700

    api

--- src/component/Demo-axios.js
+++ src/component/Demo-axios.js
@@ -1,55 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import axios from "axios";
+import api from "./api";
 function DemoAxios() {
   // GET Request
-  const [data, Setdata] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/users") //lấy dữ liệu của fake API
-      .then((res) => {
-        Setdata(res.data); // truyền dữ liệu từ setData vào res.data . nghĩa là truyền vào data đoá
-      })
-      .catch((error) => console.log(error)); // gọi lỗi ra nếu lây api hỏng
-  }, []);
-  function renderData() {
-    console.log(data); //kiểm tra đã lấy được data từ api chưa ?
-    if (data.length > 0) {
-      return data.map((value, key) => {
-        return (
-          <li key={key}>
-            <p>{value.id}</p>
-            <p>{value.name}</p>
-          </li>
-        );
-      });
-    }
-  }
+  // const [data, Setdata] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/users") //lấy dữ liệu của fake API
+  //     .then((res) => {
+  //       Setdata(res.data); // truyền dữ liệu từ setData vào res.data . nghĩa là truyền vào data đoá
+  //     })
+  //     .catch((error) => console.log(error)); // gọi lỗi ra nếu lây api hỏng
+  // }, []);
+  // function renderData() {
+  //   console.log(data); //kiểm tra đã lấy được data từ api chưa ?
+  //   if (data.length > 0) {
+  //     return data.map((value, key) => {
+  //       return (
+  //         <li key={key}>
+  //           <p>{value.id}</p>
+  //           <p>{value.name}</p>
+  //         </li>
+  //       );
+  //     });
+  //   }
+  // }
 
   //   POST Request
   // truyền dữ liệu vào api
   // muốn truyền bao nhiêu thì khai báo bấy nhiêu
-  const [input, setInput] = useState(""); //khởi đầu là ""
+  // const [input, setInput] = useState(""); //khởi đầu là ""
 
-  function handleChange(e) {
-    setInput(e.target.value); //lấy value vừa nhập
-  }
+  // function handleChange(e) {
+  //   setInput(e.target.value); //lấy value vừa nhập
+  // }
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  // function handleSubmit(e) {
+  //   e.preventDefault();
 
-    const data = {
-      name: input, // khai báo 1 phần tử input
-    };
-    axios
-      .post("https://jsonplaceholder.typicode.com/users", data)
-      .then((res) => {
-        console.log(res);
-      });
-  }
+  //   const data = {
+  //     name: input, // khai báo 1 phần tử input
+  //   };
+  //   axios
+  //     .post("https://jsonplaceholder.typicode.com/users", data)
+  //     .then((res) => {
+  //       console.log(res);
+  //     });
+  // }
 
   // DELETE Request
   // xóa 1 hoặc nhiều khỏi api bằng axios.delete
-  const [inputId, setInputId] = useState(""); // khởi đầu của input là ""
+  const [input, setInput] = useState(""); // khởi đầu của input là ""
 
   function handleChange(e) {
     setInput(e.target.value);
@@ -59,7 +60,7 @@ function DemoAxios() {
     e.preventDefault();
 
     const data = {
-      id: inputId,
+      id: input,
     };
     axios
       .delete("https://jsonplaceholder.typicode.com/users", data)
@@ -69,10 +70,20 @@ function DemoAxios() {
       });
   }
 
+  class PersonList extends Component {
+    handleSubmit(e) {
+      e.preventDefault();
+      api.delete("users/" + this.state.id).then((res) => {
+        console.log(res);
+        console.log(res.data);
+      });
+    }
+  }
+
   return (
     <>
       //POST Request
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="username"
@@ -80,7 +91,7 @@ function DemoAxios() {
           onChange={handleChange}
         />
         <button type="submit"> Click me to SUSmit !</button>
-      </form>
+      </form> */}
       // DELETE Request
       <form onSubmit={handleSubmit}>
         <input type="text" name="id" value={input} onChange={handleChange} />
