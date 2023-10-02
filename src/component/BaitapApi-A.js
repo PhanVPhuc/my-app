5d775dceb22096853f96b3b5a2e0f9108fdcfaa5
VinhPhuc <pphuc1996@gmail.com>
2023-10-02 21:38:07 +0700

    done 18.2

--- src/component/BaitapApi-A.js
+++ src/component/BaitapApi-A.js
@@ -4,11 +4,26 @@ import AxiosB from "./BaitapApi-B";
 
 function AxiosA(props) {
   const [data, setData] = useState([]);
+
+  // bai 18.1
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => {
+  //       setData(res.data);
+  //     })
+  //     .catch((error) => console.log(error));
+  // }, []);
+
+  //bai 18.2
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
-        setData(res.data);
+        if (res.data.length > 0) {
+          const randomIndex = Math.floor(Math.random() * res.data.length);
+          setData(res.data[randomIndex]);
+        }
       })
       .catch((error) => console.log(error));
   }, []);
